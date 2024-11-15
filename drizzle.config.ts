2646521a068bd2067581f03290b062a4f086df93
Xazu <Xazu001@users.noreply.github.com>
2024-11-15 19:20:01 +0100

    Works

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
-  out: "./drizzle",
+  out: "./migrations",
   schema: "./server/database/schema.ts",
   dialect: "sqlite",
   driver: "d1-http",
